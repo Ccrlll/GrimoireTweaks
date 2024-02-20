7a55da1e98a28eefe38f55f761895359a41cd0ad
Ccrlll <125846010+Ccrlll@users.noreply.github.com>
2024-02-20 17:04:41 +0800

    Update v1.5.1
    major: General Update
    more improvements under the hood

--- common/post-fs-data.sh
+++ common/post-fs-data.sh
@@ -2,3 +2,4 @@
 MODDIR=${0%/*}
 
 nohup sh $MODDIR/GrimoireTweaks/on_boot > /dev/null & 2>&1
+exit 0
--- common/service.sh
+++ common/service.sh
@@ -2,3 +2,4 @@
 MODDIR=${0%/*}
 
 nohup sh $MODDIR/GrimoireTweaks/on_system > /dev/null & 2>&1
+exit 0
